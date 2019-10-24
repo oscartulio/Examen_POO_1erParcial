df092d4199a74d5cd6049b24349fce8aab6a9e18
oscartulio <51069335+oscartulio@users.noreply.github.com>
2019-10-24 03:48:52 +0200

    Avance 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,34 @@
+import javax.lang.model.util.ElementScanner6
+import kotlin.concurrent.thread
 
 fun main() {
 
 
     println("El elevador de encuentra DETENIDO en el lobby")
     Thread.sleep(1000)
-    var piso: String?
-    var floor: Int
+    var piso: Int?
+    var estado: String
     println("Ingrese el piso al que desea ir: ")
-    piso = readLine()
-    if(piso != null){
-        floor = piso.toInt()
+    piso = readLine()?.toInt() as Int
+
+    if (piso>1){
+        estado="ARRIBA"
+    }
+    else
+    {
+        estado="ABAJO"
     }
 
-    
-
-
-
+    for(n in 1..piso) {
+    println("Estado del elevador $estado, piso $n")
+    piso++
+        Thread.sleep(1000)
+    }
 
 
 }
+
+
+
+
+
--- src/elevador.kt
+++ src/elevador.kt
@@ -5,4 +5,6 @@ open class elevador (val lobby: String = "DETENIDO", var arriba: Int, var abajo:
         return "El elevador se encuentra $lobby en el Lobby "
     }
 
+
+
 }
