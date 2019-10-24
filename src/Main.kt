292ede73c1fd2b491f103f33330483ad9973c484
oscartulio <51069335+oscartulio@users.noreply.github.com>
2019-10-24 03:13:10 +0200

    Avance

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+
+
+    println("El elevador de encuentra DETENIDO en el lobby")
+    Thread.sleep(1000)
+    var piso: String?
+    var floor: Int
+    println("Ingrese el piso al que desea ir: ")
+    piso = readLine()
+    if(piso != null){
+        floor = piso.toInt()
+    }
+
+    
+
+
+
+
+
+}
---
+++ src/elevador.kt
@@ -0,0 +1,8 @@
+
+open class elevador (val lobby: String = "DETENIDO", var arriba: Int, var abajo: Int){
+
+    override fun toString(): String {
+        return "El elevador se encuentra $lobby en el Lobby "
+    }
+
+}
